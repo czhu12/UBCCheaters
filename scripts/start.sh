c437f384fba19eded0eaf10040cf1d3a89563ea2
Your Name <you@example.com>
2015-05-13 04:46:23 -0400

    updated scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,9 @@
 db() {
-  docker run -t postgres:latest
+  docker run -P --name app_db -e POSTGRES_USER=chatty_prod -e POSTGRES_PASSWORD=chatty_prod -t postgres:latest
 }
 
 app() {
+  
 }
 
 action=$1
