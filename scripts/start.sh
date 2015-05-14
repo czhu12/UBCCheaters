f89109c9467c2b1b20f5e76dbc2ae888b2c61dab
Chris Zhu <chris@wiredhorizon.com>
2015-05-14 17:46:49 -0400

    added deployment scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,17 @@
+build() {
+  docker build -t chriszhu/ubc_cheaters_app .
+}
+
+data() {
+  docker run -v /var/lib/postgresql/data --name ubc_cheaters_data busybox
+}
+
 db() {
-  docker run -P --name app_db -e POSTGRES_USER=chatty_prod -e POSTGRES_PASSWORD=chatty_prod -t postgres:latest
+  docker run --name ubc_cheaters_db -e POSTGRES_PASSWORD=2ubc4cheaters8 -d postgres
 }
 
 app() {
-  
+  docker run -p 80:8080 -i --link ubc_cheaters_db:postgres --name ubc_cheaters_app -d chriszhu/ubc_cheaters_app
 }
 
 action=$1
